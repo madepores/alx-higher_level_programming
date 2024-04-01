44770cce3971dacb4b1dfa8ff8465cfb275a61eb
madepores <martin.barindirimana@aims.ac.rw>
2024-04-01 08:56:01 -0700

    l

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-curl -sI $1 | grep "Content-Length" | cut -d " " -f2
+# Check if URL argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send request to the URL and store response body in a temporary file
+response=$(curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}')
+
+# Display the size of the body of the response in bytes
+echo "$response"
