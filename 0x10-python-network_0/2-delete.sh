2a0b7c3912e971a95669fa5587aae4a2db3b3d9c
madepores <martin.barindirimana@aims.ac.rw>
2024-04-18 21:59:26 +0000

    l

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 # Sends a DELETE request to the URL passed as the first argument and displays the body of the respons
 curl -s -X DELETE "${1}"
-=======
-#!/usr/bin/env bash
-
-curl -sX DELETE $1 -L
->>>>>>> 96b30d2daebdaab3e1ee0809c6cde423ee178941
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-<<<<<<< HEAD
-# Sends a POST request to the passed URL, and displays the body of the response
-curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "${1}"
-=======
 
-curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
->>>>>>> 96b30d2daebdaab3e1ee0809c6cde423ee178941
+# Check if the number of arguments is correct
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 URL"
+    exit 1
+fi
+
+# Send POST request with specified parameters using curl
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
