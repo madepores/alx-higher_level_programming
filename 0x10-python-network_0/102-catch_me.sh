9eeac8fb7fcddf1880b503e2c45faf8b6298951b
madepores <martin.barindirimana@aims.ac.rw>
2024-04-18 22:19:42 +0000

    l

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me_3
