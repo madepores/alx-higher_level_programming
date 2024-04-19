5cf41bab6d14842d1af493e51d1af4b4684960ff
madepores <martin.barindirimana@aims.ac.rw>
2024-04-19 18:41:23 +0000

    l

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.parse
-import sys
+"""
+Sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.parse
+    import sys
 
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data, method='POST')
+    argv = sys.argv
+    url = argv[1]
+    email = argv[2]
+    DATA = urllib.parse.urlencode({"email": email})
+    DATA = DATA.encode('ascii')
 
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen(url, DATA) as response:
         print(response.read().decode('utf-8'))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
+
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-
-    response = requests.get(url)
-
-    print("Body response:")
-    print("\t- type:", type(response.text))
-    print("\t- content:", response.text)
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
